2e0225db8fb327f2b8e7fa6907554acb59938c48
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-03-11 16:56:53 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import ContactMe from "./ContactMe";
 
 function App() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0  ">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar  scrollbar-track-gray-400/20 scrollbar-thumb-red-400/80 ">
       <title>Saad's Portfolio</title>
 
       <Header />
