584c98fa44b762cdf63e3007980c5e0dd0199094
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-03-11 19:19:09 +0530

    Add files via upload

---
+++ src/Skills/Skill16.tsx
@@ -0,0 +1,35 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+type Props = {
+  directionLeft?: boolean;
+};
+
+function Skill({ directionLeft }: Props) {
+  return (
+    <div className="group relative flex cursor-pointer">
+      <motion.img
+         initial={{ opacity: 0, x: directionLeft ? -100 : 100 }}
+         transition={{ duration: 1 }}
+         whileInView={{ opacity: 1, x: 0 }}
+         className="rounded-full border border-gray-500 object-cover w-16 h-16 sm:w-20 sm:h-20 md:h-24 md:w-24   filter group-hover:grayscale " // transition duration-300 ease-in-out
+         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-FSykwiFxV4EfnSiW5Qq-BNlF8AqY9rpnkA&usqp=CAU"
+        
+      />
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-16 h-16 sm:w-20 sm:h-20 md:h-24 md:w-24  rounded-full z-0">
+        <div className="flex items-center justify-center h-full">
+          <p className="text-3xl font-bold text-black opacity-100">85%</p>
+        </div>
+      </div>
+
+    {/* Try */}
+
+    
+
+
+      
+    </div>
+  );
+}
+
+export default Skill;
