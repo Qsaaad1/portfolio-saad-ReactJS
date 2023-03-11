56a8c4f52c4e8ee08e131b5463db0638a128c3e8
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-03-11 17:34:52 +0530

    Update About.tsx

--- src/About.tsx
+++ src/About.tsx
@@ -28,15 +28,15 @@ function About({}: Props) {
         }}
         viewport={{ once: true }}
         src="https://user-images.githubusercontent.com/101616957/223126135-d0e84b66-3e60-40ac-abfc-88a18cfbd0cf.jpg"
-        className=" z-20 -mb-20 md:mb-0 flex-shrink-0  w-32 h-32 mt-32  sm:w-64 sm:h-64 rounded-full object-cover  md:rounded-lg md:w-64 md:h-96 xl:h-[500px] xl:w-[500px] "
+        className=" z-20 -mb-20 md:mb-0 flex-shrink-0  w-32 h-32 mt-10  sm:w-64 sm:h-64 rounded-full object-cover  md:rounded-lg md:w-64 md:h-96 xl:h-[450px] xl:w-[450px] "
       />
 
-      <div className="space-y-10 pt-20 px-0 md:px-10">
-        <h4 className="text-2xl sm:text-3xl font-semibold">
+      <div className="sm:space-y-10 sm:pt-20 px-0 md:px-10">
+        <h4 className=" text-lg sm:text-3xl font-semibold">
           Here is a <span className="underline decoration-red-400">little</span>{" "}
           about me...
         </h4>
-        <p>
+        <p className="text-base sm:text-lg">
           My name is Saad Ahmed Qureshi and I am a highly motivated individual
           with a passion for success 💯. I bring a wealth of knowledge and
           experience to any project. I am a strong leader and team player, able
