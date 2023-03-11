f1e53e73002574bbf2c09f53f3f50df687c8da7f
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-03-11 20:01:35 +0530

    Update Skills.tsx

--- src/Skills.tsx
+++ src/Skills.tsx
@@ -36,7 +36,7 @@ function Skills({}: Props) {
         Hover over to learn more
       </h3>
 
-      <div className="grid grid-cols-4 sm:grid-cols-4 gap-5 py-64 md:py-52 h-screen">
+      <div className="grid grid-cols-3 sm:grid-cols-4 gap-5 py-64 md:py-52 h-screen">
         <Skill/>
         <Skill12/>
         <Skill2/>
