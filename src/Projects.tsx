690bb11fc9222949137bacdd8cda989dbdffc673
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-03-11 19:38:32 +0530

    Update Projects.tsx

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -13,7 +13,7 @@ const Projects = (props: Props) => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className=" relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0  h-screen overflow-y-hidden"
+      className=" relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0  h-screen overflow-y-hidden "
     >
       <h3 className="absolute top-24 uppercase tracking-[24px] text-gray-500 text-2xl ">
         Projects
