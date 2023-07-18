97ce1a9f9300cb1d5dc35e3ad8dfe56f44630b60
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-07-19 01:05:54 +0530

    Update EducationCard.tsx

--- src/EducationCard.tsx
+++ src/EducationCard.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 function EducationCard({}: Props) {
   return (
-    <article className="flex flex-col rounded-lg p-10 items-center space-y-7 flex-shrink-0 w-[600px] md:w-[700px] xl:w-[900px] snap-center bg-[#292929] hover:opacity-100 opacity-80 cursor-pointer transition-opacity duration-200 overflow-hidden">
+    <article className=" flex flex-col rounded-lg p-2 items-center space-y-10 flex-shrink-0 w-[400px] sm:w-[600px] md:w-[700px] xl:w-[900px] snap-center bg-[#292929] hover:opacity-100 opacity-80 cursor-pointer transition-opacity duration-200 overflow-hidden">
       <motion.img
         initial={{ y: -100, opacity: 0 }}
         transition={{ duration: 1.2 }}
@@ -16,14 +16,14 @@ function EducationCard({}: Props) {
       />
 
       <div className="px-0 space-y-4 md:px-10 ">
-        <h4 className="text-4xl font-light">Bachelor of Technology</h4>
-        <p className="text-2xl font-light">
-          Shri Vaishnav Vidyapeeth Vishwavidyalaya (SVVV) 
+        <h4 className="sm:text-4xl font-light">Bachelor of Technology</h4>
+        <p className=" sm:text-2xl font-light">
+          Shri Vaishnav Vidyapeeth Vishwavidyalaya
         </p>
-        <p className="text-2xl font-light">
-          Specilization in Mobile Application
+        <p className="sm:text-2xl font-light">
+          Specialization in Mobile Application
         </p>
-        <ul className="list-disc space-y-4 ml-5 text-lg ">
+        <ul className="list-disc space-y-4 ml-5 sm:text-lg ">
           <li>2020-2024</li>
           <li>CGPA : 7.6/10</li>
         </ul>
