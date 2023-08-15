392b271ee36d1b1f2264ea99fcf479ef43b8c2bc
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-08-15 19:14:28 +0530

    Update Skill5.tsx

--- src/Skills/Skill5.tsx
+++ src/Skills/Skill5.tsx
@@ -18,7 +18,7 @@ function Skill({ directionLeft }: Props) {
       />
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-16 h-16 sm:w-20 sm:h-20 md:h-24 md:w-24  rounded-full z-0">
         <div className="flex items-center justify-center h-full">
-          <p className="text-3xl font-bold text-black opacity-100">75%</p>
+          <p className="text-3xl font-bold text-black opacity-100">85%</p>
         </div>
       </div>
 
